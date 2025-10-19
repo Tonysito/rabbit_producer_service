3f2aec3200f7c1ff18e58449525c090e8e326ef9
Anthony Aldair Espinoza Perez <anthonyaldairespinozaperez@outlook.com>
2025-10-19 07:50:18 +0200

    Actualizando a versión 1.0.6

--- src/main/java/org/cibertec/RabbitProducerServiceAdminApplication.java
+++ src/main/java/org/cibertec/RabbitProducerServiceAdminApplication.java
@@ -3,7 +3,7 @@ package org.cibertec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.cibertec")
 public class RabbitProducerServiceAdminApplication {
 
 	public static void main(String[] args) {
